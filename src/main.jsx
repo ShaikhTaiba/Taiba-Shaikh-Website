ccacb05de98ce3d4846a336773e6c8895a1ffd35
ShaikhTaiba <taibashaikh025@gmail.com>
2024-11-24 21:56:39 +0530

    Update base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename="/Taiba-Shaikh-Website">
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
